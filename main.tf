3289bdd46f729e8f2b2b030bdd1d500556327dc6
Khaled-Ghazy <161209615+KhaledGhazy3@users.noreply.github.com>
2024-03-29 15:04:19 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,62 @@
- #Creating VPC
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0f403e3180720dd7e"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.demosubnet1.id
+  vpc_security_group_ids = [ aws_security_group.example.id ]
+  key_name = "test_key"
+
+  tags = {
+    Name = "Python-App_EC2"
+  }
+}
+
+
+resource "aws_security_group" "example" {
+  
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.demovpc.id
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+# Configure eip resource
+resource  "aws_eip" "my_eip"{
+    vpc = true
+}
+
+# Associate eip to the ec2
+resource "aws_eip_association" "associate"{
+    instance_id=aws_instance.my_ec2.id
+    allocation_id=aws_eip.my_eip.id
+}
+
+
+# output/print the eip
+output "instance_public_ip" {
+  value = aws_eip.my_eip.public_ip
+}
+
+
+
+#Creating VPC
 resource "aws_vpc" "demovpc" {
   cidr_block       = var.vpc_cidr
   instance_tenancy = "default"

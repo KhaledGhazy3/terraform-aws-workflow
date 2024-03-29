7f5efd009ffe06dbd7d504936990669b99b7e821
Khaled-Ghazy <161209615+KhaledGhazy3@users.noreply.github.com>
2024-03-29 16:44:54 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "my_ec2" {
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.demosubnet1.id
   vpc_security_group_ids = [ aws_security_group.example.id ]
-  aws_key_pair = "vockey"
+  key_name= "vockey"
 
   tags = {
     Name = "Python-App_EC2"

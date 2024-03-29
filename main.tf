dcaadc143fc2f7fcef409267ea1dd62d3dc8b5b3
Khaled-Ghazy <161209615+KhaledGhazy3@users.noreply.github.com>
2024-03-29 15:06:56 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0f403e3180720dd7e"
+  ami           = "ami-080e1f13689e07408"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.demosubnet1.id
   vpc_security_group_ids = [ aws_security_group.example.id ]
